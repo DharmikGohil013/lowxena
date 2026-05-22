db86b039ae8cc991919e856e35709073897cc844
DharmikGohil013 <dharmikgohil395003@gmail.com>
2026-05-22 11:03:22 +0530

    icon update

--- client/src/pages/PracticeGame.jsx
+++ client/src/pages/PracticeGame.jsx
@@ -824,10 +824,7 @@ function PracticeGame() {
       {/* Top Bar */}
       <div className="game-top-bar">
         <button className="control-btn" onClick={() => setShowScoreboard(!showScoreboard)} title="Scoreboard">
-          <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-            <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
-            <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path>
-          </svg>
+          <BarChart3 size={22} />
         </button>
 
         <div className="turn-indicator">
